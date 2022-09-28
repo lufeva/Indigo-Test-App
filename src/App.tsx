c0072dbfa24393b6d6aa50b79bdb74442fb5b970
Luisa Valencia <luisafer144@gmail.com>
2022-09-28 18:55:21 -0500

    feature(markdown-builder): add initial set up

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <textarea onChange={(e) => console.log(e.target.value)} rows={5} />
+    </div>
+  );
+}
+
+export default App;
